994aab29d7200f8dc0bc6add1382c7b40bd42e2e
Danailbd <danailbd@danailbd-Latitude-E5400.(none)>
2014-03-13 19:09:46 +0200

    Adding some old stuff

---
+++ week3/1st/string_test.py
@@ -0,0 +1,16 @@
+import unittest
+import string_utils
+
+class StringUtilsTests(unittest.TestCase):
+
+    def test_lines(self):
+        self.assertEqual(["aaa", "bbb", "ccc"], string_utils.lines("aaa\nbbb\nccc"))
+
+    def test_unlines(self):
+        self.assertEqual("aa\nvv\n\dsf\nder", string_utils.unlines(["aa", "vv", "\dsf", "def"]))
+
+    def test_words(self):
+        self.assertEqual(["aa", "vv", "\dsf", "def"], string_utils.words("aa vv \dsf der"))
+
+    def test_unwords(self):
+        self.assertEqual("aa\nvv\n\dsf\nder", string_utils.unwords(["aa", "vv", "\dsf", "def"]))
